3d8ce4409a6f20faa9e8af2629b51c367f700bc8
Kartik Arora <kartikaroraofficial01@gmail.com>
2024-01-12 03:13:08 +0530

    .

--- Searching/Binary.java
+++ Searching/Binary.java
@@ -5,5 +5,19 @@ package Searching;
  */
 public class Binary {
 
+    static int DoBinarySearchIteratively(int[] arr, int key) {
+        int right = 0;
+        int left = arr.length -1;
+        while(right < left) {
+            int mid = (right + left) /2;
+            if(key > arr[mid]) right = mid + 1;
+            else if (key == arr[mid]) return mid;
+            else left = mid; 
+        }
+        return -1;
+    }
     
+    public static void main(String[] args) {
+        
+    }
 }
